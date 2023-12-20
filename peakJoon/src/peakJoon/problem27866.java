3df8f37eecc717fb5374d6e16a0bd0859a06a399
JungSeungGeun <wjd15sheep@naver.com>
2023-12-20 14:43:10 +0900

    test

---
+++ peakJoon/src/peakJoon/problem27866.java
@@ -0,0 +1,19 @@
+package peakJoon;
+
+import java.util.Scanner;
+
+public class problem27866 {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		String st = sc.next();
+		int a = sc.nextInt();
+		sc.close();
+
+		System.out.println(st.charAt(a - 1));
+
+	}
+
+}
